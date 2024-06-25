a5cabbef4b2f634dae6e62ead3f582d9438710e1
LiHaonan <1974267856@qq.com>
2024-06-25 21:08:26 +0800

    Add HelloGit_B

---
+++ Assets/Script/HelloGit_B.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloGit_B : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HelloGit_B");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
